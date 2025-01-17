98f41c7fa910ea562fa37891e3e757ac8e6cbdd8
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2025-01-17 23:38:30 +0800

    chore: remove static items in Cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -5,38 +5,6 @@ import { type CartItem } from "@/store/features/cartSlice";
 import CartItems from "@/pages/Cart/components/CartItems";
 import CartComputation from "@/pages/Cart/components/CartComputation";
 
-const items = [
-  {
-    id: "the-gramercy-tavern-burger-4-pack",
-    img: "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/137148/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-15.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
-    name: "Gramercy Tavern",
-    dsc: "The Gramercy Tavern Burger - 4 Pack",
-    price: 99,
-    rate: 5,
-    country: "New York, NY",
-    qty: 3,
-  },
-  {
-    id: "choose-your-own-chicago-deep-dish-pizza-4-pack",
-    img: "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/89948/chicago-deep-dish-pizza-4-pack.49927daafa8c147fe9bb2a113e56668e.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
-    name: "My Pi Pizza",
-    dsc: "Chicago Deep Dish Pizza - 4 Pack",
-    price: 129,
-    rate: 5,
-    country: "Chicago, IL",
-    qty: 1,
-  },
-  {
-    id: "bo-ssam-dinner-for-4-6",
-    img: "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/110906/bo-ssam-dinner-for-4.c4a32e8801e2f0283e0565bbe8493149.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
-    name: "Momofuku",
-    dsc: "Half Bo Ssäm Dinner for 4-6",
-    price: 169,
-    rate: 4,
-    country: "New York, NY",
-    qty: 1,
-  },
-];
 
 const Cart = () => {
   const dispatch = useAppDispatch();
