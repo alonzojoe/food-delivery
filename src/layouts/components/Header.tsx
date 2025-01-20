03ef18b89a2d08be57433a93edb5fa3710ecd1dd
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2025-01-20 22:59:59 +0800

    feat: display authenticated user in Header component, adjust name add ellipsis if name is too long

--- src/layouts/components/Header.tsx
+++ src/layouts/components/Header.tsx
@@ -2,23 +2,29 @@ import { RiMenu5Fill } from "react-icons/ri";
 import { FaRegUserCircle } from "react-icons/fa";
 import { FaCaretDown } from "react-icons/fa";
 import { IoMdCart } from "react-icons/io";
+import { useAppSelector } from "@/store/store";
+import { capitalizedFirstLetter } from "@/libs/utils";
 
 const Header = () => {
+  const { user } = useAppSelector((state) => state.auth);
+
   return (
     <div className="flex items-center justify-between w-full">
-      <div className="bg-secondary p-3 rounded-lg">
+      <div className="bg-secondary p-3 rounded-lg flex-shrink-0">
         <RiMenu5Fill className="text-xl text-textDark" />
       </div>
       <div className="flex justify-center items-center gap-2">
-        <span>
+        <span className="flex-shrink-0">
           <FaRegUserCircle className="text-xl text-primary" />
         </span>
-        <span className="text-textDark font-semibold text-xl">Hello, Joe</span>
-        <span>
+        <span className="text-textDark font-semibold text-xl overflow-hidden text-ellipsis whitespace-nowrap max-w-[150px]">
+          Hello, {capitalizedFirstLetter(user?.given_name || "Joe")}
+        </span>
+        <span className="flex-shrink-0">
           <FaCaretDown className="text-xl text-primary" />
         </span>
       </div>
-      <div className="bg-secondary p-3 rounded-lg">
+      <div className="bg-secondary p-3 rounded-lg flex-shrink-0">
         <IoMdCart className="text-xl text-textDark" />
       </div>
     </div>
