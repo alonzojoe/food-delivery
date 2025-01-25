f8d24a73fb4b875b1feeafacca7b74e2c624521b
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-01-25 11:57:18 +0800

    style: add delivery timeline  ui component

--- src/pages/Delivery/Delivery.tsx
+++ src/pages/Delivery/Delivery.tsx
@@ -1,6 +1,7 @@
 import MapImg from "@/assets/images/maps.png";
 import Card from "@/components/UI/Card";
 import { MdOutlineSportsMotorsports } from "react-icons/md";
+import { IoCall } from "react-icons/io5";
 const Delivery = () => {
   return (
     <div
@@ -42,6 +43,52 @@ const Delivery = () => {
               <button className="py-3 px-5 bg-primary rounded-2xl text-white">
                 Details
               </button>
+            </div>
+          </div>
+        </Card>
+      </div>
+      <div className="container absolute bottom-5 p-5">
+        <Card>
+          <div className=" bg-white p-5 ro rounded-lg">
+            <div className="flex gap-3 items-center pb-4">
+              <ol className="relative border-s border-gray-200 dark:border-textMuted">
+                <li className="mb-10 ms-4">
+                  <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-primary dark:bg-primary"></div>
+                  <h3 className="font-bold text-textDark">
+                    Mang Inasal - Dolores CSFP
+                  </h3>
+                  <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                    Order Place • 10:00 AM
+                  </time>
+                </li>
+                <li className="mb-0 ms-4">
+                  <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-primary dark:bg-primary"></div>
+                  <h3 className="font-bold text-textDark">Del Rosario CSFP</h3>
+                  <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                    Home • ETA 10:30 AM
+                  </time>
+                </li>
+              </ol>
+            </div>
+            <div className="pt-5 flex items-center justify-between">
+              <div className="flex gap-3 items-center pb-4">
+                <span className="bg-primary rounded-full p-3">
+                  <MdOutlineSportsMotorsports className="text-white text-xl" />
+                </span>
+                <div>
+                  <h3 className="text-textDark font-semibold">
+                    Juan Dela Cruz
+                  </h3>
+                  <span className="text-textDark">
+                    <span className="text-textMuted text-md">
+                      Rider • +639051234567
+                    </span>
+                  </span>
+                </div>
+              </div>
+              <span className="p-3 bg-green-500 rounded-full">
+                <IoCall className="text-white text-xl" />
+              </span>
             </div>
           </div>
         </Card>
