fb11781b21b80857ff60df984dc75dda7717be74
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-01-27 16:58:02 +0800

    style: add button Place Order, fixed bottom with total amount

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -2,16 +2,16 @@ import { TbArrowNarrowLeft } from "react-icons/tb";
 
 const Checkout = () => {
   return (
-    <div className="container !px-0 my-1  h-dvh relative">
+    <div className="container !px-0  h-dvh relative">
       <div className="border-2 shadow-md border-white border-b-primary p-3 flex items-center gap-3 pb-5 relative">
         <span>
           <TbArrowNarrowLeft className="text-primary text-2xl" />
         </span>
         <div>
-          <span className="text-textDark text-lg !leading-tight block">
+          <span className="text-textDark text-lg font-semibold !leading-tight block">
             Checkout
           </span>
-          <span className="text-textMuted !leading-tight">
+          <span className="text-textMuted text-md !leading-tight">
             Del Rosario CSFP
           </span>
         </div>
@@ -42,7 +42,20 @@ const Checkout = () => {
           </span>
         </div>
       </div>
-      <div className=""></div>
+      <div className="bg-white w-full border-t border-text-muted p-5 fixed bottom-0">
+        <div className="flex justify-between items-center text-md mb-2">
+          <h2 className="font-bold">
+            Total <small className="text-gray-400">(inc. VAT)</small>
+          </h2>
+          <p className="font-bold">
+            <small className="text-primary">$</small>
+            129.00
+          </p>
+        </div>
+        <button className="bg-primary p-3 text-lg font-semibold text-white rounded-xl w-full mb-3">
+          Place Order
+        </button>
+      </div>
     </div>
   );
 };
