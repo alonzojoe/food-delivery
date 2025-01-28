3f77924f2bce57078cf034a0ee99d9b42c683bd5
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-01-28 16:59:58 +0800

    feat: replace search state to url params, it retains even user refreshe the pages

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,36 +8,49 @@ import SekeletonMeal from "@/pages/Home/components/SekeletonMeal";
 import Meals from "@/pages/Home/components/Meals";
 import useDebounce from "@/hooks/useDebounce";
 import { searchMeal } from "@/store/features/mealSlice";
+import { useSearchParams } from "react-router-dom";
+
 const Home = () => {
-  const [search, setSearch] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams({ keyword: "" });
+
+  const keyword = searchParams.get("keyword") || "";
   const [selectedCategory, setSelectedCategory] = useState("/burgers");
   const dispatch = useAppDispatch();
   const { isLoading, meals } = useAppSelector((state) => state.meal);
 
-  const debounceSearch = useDebounce(search);
+  const debounceSearch = useDebounce(keyword);
 
   useEffect(() => {
+    if (!debounceSearch) return;
     dispatch(searchMeal({ keyword: debounceSearch }));
     console.log("debounceSearch:", debounceSearch);
   }, [debounceSearch, dispatch]);
 
   useEffect(() => {
-    setSearch("");
     dispatch(fetchMeals(selectedCategory));
   }, [selectedCategory, dispatch]);
 
   return (
     <>
       <h1 className="my-5 text-3xl font-bold text-center !leading-snug">
-        What do you want to <span className="text-primary">Eat</span>?
+        What do you want to <span className="text-primary">Eat</span>?{" "}
+        {debounceSearch}
       </h1>
       <div className="bg-secondary w-full p-4 flex items-center rounded-xl relative">
         <input
           type="text"
           className="bg-transparent text-lg p-1 font-semibold ring-0 border-0 outline-none w-full placeholder:opacity-45"
           placeholder="Search your food"
-          onChange={(e) => setSearch(e.target.value)}
-          value={search}
+          onChange={(e) =>
+            setSearchParams(
+              (prev) => {
+                prev.set("keyword", e.target.value);
+                return prev;
+              },
+              { replace: true }
+            )
+          }
+          // value={keyword}
         />
         <IoSearch className="text-2xl bg-transparent absolute right-3 text-textDark opacity-85" />
       </div>
