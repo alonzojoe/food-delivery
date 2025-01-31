ac10459afb08bdbcd1a93a368049200d20ed9257
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-01-31 14:51:12 +0800

    feat: make tab dynamic create logic for class binding of active class

--- src/pages/StartUp/StartUp.tsx
+++ src/pages/StartUp/StartUp.tsx
@@ -4,6 +4,7 @@ import { jwtDecode } from "jwt-decode";
 import { useAppDispatch } from "@/store/store";
 import { setUser } from "@/store/features/authSlice";
 import Hero from "@/assets/images/scoot.png";
+import { useState } from "react";
 
 type GoogleAuthUser = {
   sub: string;
@@ -32,6 +33,8 @@ export type AuthUser = {
 const StartUp = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
+  const [selectedTab, setSelectedTab] = useState(1);
+
   const handleSuccess = (response: CredentialResponse) => {
     console.log("response", response);
     if (response.credential) {
@@ -62,12 +65,17 @@ const StartUp = () => {
         <div className="mt-2">
           {/* tab */}
 
-          <div className="text-sm font-medium text-center text-gray-500 border-b border-textDark">
+          <div className="text-sm font-medium text-center text-gray-500 border-b ">
             <ul className="flex flex-wrap -mb-px">
               <li className="me-2">
                 <a
                   href="#"
-                  className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-primary hover:border-primary"
+                  onClick={() => setSelectedTab(1)}
+                  className={`inline-block p-4 border-b-2 ${
+                    selectedTab === 1
+                      ? "text-primary border-primary rounded-t-lg active"
+                      : "hover:text-primary hover:border-primary"
+                  }`}
                 >
                   Login
                 </a>
@@ -75,7 +83,12 @@ const StartUp = () => {
               <li className="me-2">
                 <a
                   href="#"
-                  className="inline-block p-4 text-primary border-b-2 border-primary rounded-t-lg active"
+                  onClick={() => setSelectedTab(2)}
+                  className={`inline-block p-4 border-b-2 ${
+                    selectedTab === 2
+                      ? "text-primary border-primary rounded-t-lg active"
+                      : "hover:text-primary hover:border-primary"
+                  }`}
                   aria-current="page"
                 >
                   Signup
@@ -84,38 +97,48 @@ const StartUp = () => {
             </ul>
           </div>
 
-          <div className="mt-3">
-            <form className="mx-auto">
-              <div className="mb-5">
-                <label
-                  htmlFor="username"
-                  className="text-primary block text-sm font-medium mb-2"
-                >
-                  Username
-                </label>
-                <input
-                  className="w-full border border-primary rounded-lg px-3 py-2 focus:outline-none ring-0 disabled:opacity-15"
-                  type="text"
-                  name="username"
-                  id="username"
-                />
-              </div>
-              <div className="mb-5">
-                <label
-                  htmlFor="password"
-                  className="text-primary block text-sm font-medium mb-2"
-                >
-                  Password
-                </label>
-                <input
-                  className="w-full border border-primary rounded-lg px-3 py-2 focus:outline-none ring-0 disabled:opacity-15"
-                  type="password"
-                  name="password"
-                  id="password"
-                />
-              </div>
-            </form>
-          </div>
+          {selectedTab === 1 && (
+            <div className="mt-3">
+              <form className="mx-auto">
+                <div className="mb-3">
+                  <label
+                    htmlFor="username"
+                    className="text-primary block text-sm font-medium mb-2"
+                  >
+                    Username
+                  </label>
+                  <input
+                    className="w-full border border-primary rounded-lg px-3 py-2 focus:outline-none ring-0 disabled:opacity-15"
+                    type="text"
+                    name="username"
+                    id="username"
+                  />
+                </div>
+                <div className="mb-5">
+                  <label
+                    htmlFor="password"
+                    className="text-primary block text-sm font-medium mb-2"
+                  >
+                    Password
+                  </label>
+                  <input
+                    className="w-full border border-primary rounded-lg px-3 py-2 focus:outline-none ring-0 disabled:opacity-15"
+                    type="password"
+                    name="password"
+                    id="password"
+                  />
+                </div>
+                <div className="mb-3">
+                  <button className="bg-primary p-4 text-sm font-normal w-full text-white rounded-2xl">
+                    Login
+                  </button>
+                </div>
+                <div className="text-center text-textDark font-medium text-md">
+                  or
+                </div>
+              </form>
+            </div>
+          )}
 
           {/* tab */}
         </div>
