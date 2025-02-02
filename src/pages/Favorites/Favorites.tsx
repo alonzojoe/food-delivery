2c8b07f2188a8df16fecdeb6d197ccd2afc829fe
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2025-02-02 23:49:53 +0800

    refactor: Implement InputSearch component supplied proper props and replace search_keyword to keyword

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -7,12 +7,14 @@ import { setFavorites, addOrRemoveItem } from "@/store/features/favoriteSlice";
 import { type Meal } from "@/store/features/mealSlice";
 import useLocalStorage from "@/hooks/useLocalStorage";
 import useDebounce from "@/hooks/useDebounce";
+import InputSearch from "@/components/UI/InputSearch";
+
 const Favorites = () => {
   const [searchParams, setSearchParams] = useSearchParams({
     search_keyword: "",
   });
 
-  const search_keyword = searchParams.get("search_keyword") || "";
+  const search_keyword = searchParams.get("keyword") || "";
   const debounceSearch = useDebounce(search_keyword);
   const dispatch = useAppDispatch();
   const { meals } = useAppSelector((state) => state.favorites);
@@ -52,24 +54,7 @@ const Favorites = () => {
 
   return (
     <div className="container !px-0 my-5  h-dvh">
-      <div className="bg-secondary w-full p-4 flex items-center rounded-xl relative mb-3">
-        <input
-          type="text"
-          className="bg-transparent text-lg p-1 font-semibold ring-0 border-0 outline-none w-full placeholder:opacity-45"
-          placeholder="Search your favorite food"
-          onChange={(e) =>
-            setSearchParams(
-              (prev) => {
-                prev.set("search_keyword", e.target.value);
-                return prev;
-              },
-              { replace: true }
-            )
-          }
-          value={search_keyword}
-        />
-        <IoSearch className="text-2xl bg-transparent absolute right-3 text-textDark opacity-85" />
-      </div>
+      <InputSearch setParams={setSearchParams} keywordValue={search_keyword} />
 
       <h2 className="text-primary text-2xl font-bold">Favorites</h2>
       <div className="mt-5">
