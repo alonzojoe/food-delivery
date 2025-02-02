fffac2a443bcc7ad75b5555595f7e07b41c1544b
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2025-02-02 23:41:42 +0800

    refactor: replace current SearchInput with the created custom component and pass the propert props needed

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,6 +9,7 @@ import Meals from "@/pages/Home/components/Meals";
 import useDebounce from "@/hooks/useDebounce";
 import { searchMeal } from "@/store/features/mealSlice";
 import { useSearchParams } from "react-router-dom";
+import InputSearch from "@/components/UI/InputSearch";
 
 const Home = () => {
   const [searchParams, setSearchParams] = useSearchParams({ keyword: "" });
@@ -33,27 +34,9 @@ const Home = () => {
   return (
     <>
       <h1 className="my-5 text-3xl font-bold text-center !leading-snug">
-        What do you want to <span className="text-primary">Eat</span>?{" "}
-        {debounceSearch}
+        What do you want to <span className="text-primary">Eat</span>?
       </h1>
-      <div className="bg-secondary w-full p-4 flex items-center rounded-xl relative">
-        <input
-          type="text"
-          className="bg-transparent text-lg p-1 font-semibold ring-0 border-0 outline-none w-full placeholder:opacity-45"
-          placeholder="Search your food"
-          onChange={(e) =>
-            setSearchParams(
-              (prev) => {
-                prev.set("keyword", e.target.value);
-                return prev;
-              },
-              { replace: true }
-            )
-          }
-          value={keyword}
-        />
-        <IoSearch className="text-2xl bg-transparent absolute right-3 text-textDark opacity-85" />
-      </div>
+      <InputSearch setParams={setSearchParams} keywordValue={keyword} />
       <>
         <h3 className="my-5 text-xl font-semibold">Categories</h3>
         <Categories
